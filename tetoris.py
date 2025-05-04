1309788155cf4292e336808df84d0be1657f3c0b
JT <jacktayl710@gmail.com>
2025-05-04 12:59:56 -0600

    Added the Files from VS code to keep them on  githuh, it was already completed

---
+++ tetoris.py
@@ -0,0 +1,63 @@
+import os
+import sys
+print("E:\Code\Tetoris", os.getcwd())
+import PIL
+import tkinter as tk
+from itertools import count
+from PIL import Image, ImageTk
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+
+class AnimatedGIF(tk.Label):
+    def __init__(self, master, gif_path, delay=100):
+        super().__init__(master, bg='black') # set label background to black
+        gif_path = resource_path(gif_path)
+        self.gif = Image.open(gif_path) # FIXED: overwrite gif_path with full path
+        self.frames = []
+        self.delay = delay
+
+
+        try:
+            for frame in count(1):
+                self.frames.append(ImageTk.PhotoImage(self.gif.copy()))
+                self.gif.seek(frame)
+        except EOFError:
+            pass # End of sequence
+
+        
+        self.index = 0
+        self.update_frame()
+
+
+    def update_frame(self):
+        if self.frames:
+            self.config(image=self.frames[self.index])
+            self.index = (self.index + 1) % len(self.frames)
+            self.after(self.delay, self.update_frame)
+
+
+# create main window
+root = tk.Tk()
+root.title("Tetoris")
+root.geometry("400x400") # this is the resolution I want, don't question me
+root.resizable(False, False) # Lock window size
+root.configure(bg='black') # window background
+
+
+# Display the GIF
+gif_label = AnimatedGIF(root, "tetoris.gif", delay=100)
+gif_label.pack(expand=True)
+
+
+# Start the program
+root.mainloop()
